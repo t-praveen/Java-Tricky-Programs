3d9741481bd6a14e9747a744de07757f809f5165
T Praveen <62693938+t-praveen@users.noreply.github.com>
2021-05-19 18:39:26 +0530

    Add files via upload

---
+++ TrickyProgram10.java
@@ -0,0 +1,17 @@
+//Program to print a message without using a semi-colon //
+
+package Tricky;
+
+public class TrickyProgram10 {
+
+	public static void main(String[] args) {
+		
+		if(System.out.printf("Hello , I am Praveen .") == null)
+		{
+			
+		}
+		
+
+	}
+
+}
