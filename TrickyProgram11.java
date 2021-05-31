842c908388b80f3cd8930fc5282fe3f17483af88
T Praveen <62693938+t-praveen@users.noreply.github.com>
2021-05-31 14:04:33 +0530

    Add files via upload

---
+++ TrickyProgram11.java
@@ -0,0 +1,15 @@
+//Program to print 1 to 100 without using numbers in the code . //
+
+package Tricky;
+
+public class TrickyProgram11 {
+
+	public static void main(String[] args) {
+		
+		char ch = 'A' / 'A' ;
+		String s = "..........";
+		for(int i = ch ; i <= (s.length() * s.length()) ; i++)
+				System.out.println(i);
+	}
+
+}
